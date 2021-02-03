ce79068e52a33bcbbd0626cd7dd8686c14de107c
Nicholas Levenson <nicklevenson@gmail.com>
2021-02-03 09:49:06 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Add your functions here
+
+function map(src, fn) {
+  let newA = []
+  src.forEach(e => {
+    newA.push(fn(e))
+  })
+  return newA
+}
+
+function reduce(src, fn, start) {
+   
+  if (start != null) {
+    return fn(src, start)
+  }else {
+    return fn(src)
+  }
+}
